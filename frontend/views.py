bef1b6f7572b38d6048f5ae449bf0e872e5b602b
Urfenox <cristopher.caceres05@inacapmail.cl>
2024-11-15 13:50:14 -0300

    ejemplo de muestra de datos

--- frontend/views.py
+++ frontend/views.py
@@ -4,4 +4,12 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request, "frontend/index.html")
+    return render(request, "frontend/index.html", {
+        "temperatura": "24°C",
+        "humedad": "17%",
+        "lux": "329",
+        "actuadores": {
+            "cortina": "60%",
+            "luz": "30%"
+        }
+    })
